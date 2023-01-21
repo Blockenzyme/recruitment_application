c3750d31e50fd1386d34f1c6eceb2de4d919df51
Srutip04 <subuhishruti75@gmail.com>
2023-01-22 03:20:21 +0530

    landing page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Landing from './pages/Landing';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Blockenzyme</h1>
+     <Landing/>
     </div>
   );
 }
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        {/* <img src={logo} alt="Blockenzyme" className="logo" /> */}
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+             <span>Recruitment</span> application
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
