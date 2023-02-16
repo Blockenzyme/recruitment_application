2d7e65ac1b0ed4362901865d5b5550953dde9e7f
Shruti <73495200+Srutip04@users.noreply.github.com>
2023-02-17 01:33:44 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,16 +4,20 @@ const {Schema} = mongoose;
 const userSchema = new Schema({
     name: {
        type: String,
-       required:true,
+       required: [true, 'Please provide name'],
+       minlength: 3,
+       maxlength: 20,
+       trim: true,
     },
     email: {
         type:String,
-        required:true,
+        required: [true, 'Please provide email'],
         unique:true
     },
     password:{
         type:String,
-        required:true
+        required: [true, 'Please provide password'],
+        minlength: 6,
     },
     role:{
         type:String,
