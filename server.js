aa4c9ec5dc81d2da0cb6c65244dab5fdb8038144
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-03-24 21:22:48 +0530

    Job Routes Created

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ import UserRoutes from "./routes/userRoutes.js";
 import RecruiterAuthRoutes from "./routes/recruiterAuthRoutes.js";
 import RecruiterRoutes from "./routes/recruiterRoutes.js";
 import passportConfig from "./config/passport.js";
+import jobs from "./routes/jobs.js"
 
 import notFoundMiddleWare from "./middlewares/not-found.js";
+import errorHandleMiddleware from "./middlewares/error-handler.js"
 
 const app = express();
 dotenv.config();
@@ -31,9 +33,10 @@ app.get("/", (req, res) => {
   res.send("welcome");
 });
 
+
 // app.use(notFoundMiddleWare);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const url = process.env.MONGO_URL;
 
 //The code-snippet of 'Initializing Session' below should be at this place only
@@ -59,10 +62,16 @@ const start = async () => {
 };
 
 passportConfig(passport);
+//jobs routes
+app.use('/api/v1/jobs',jobs);
+app.use(notFoundMiddleWare);
+app.use(errorHandleMiddleware);
+
 
 app.use(UserAuthRoutes);
 app.use(UserRoutes);
 app.use(RecruiterAuthRoutes);
 app.use(RecruiterRoutes);
+
 
 start();
