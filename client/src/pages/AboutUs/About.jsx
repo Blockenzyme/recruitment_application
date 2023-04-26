28ed55821d9ab0a18bee1b4b3a373e9273258404
Srutip04 <subuhishruti75@gmail.com>
2023-04-26 15:56:53 +0530

    refactoring

--- client/src/pages/AboutUs/About.jsx
+++ client/src/pages/AboutUs/About.jsx
@@ -3,9 +3,8 @@ import { Row, Col, Container } from "reactstrap";
 import about_1 from "../../assets/img1.png";
 import about_2 from "../../assets/img2.jpg";
 import about_3 from "../../assets/img3.jpg";
-import Footer from '../../components/footer';
+import Footer from "../../components/footer";
 import "./About.styles.css";
-
 
 function About() {
   return (
@@ -14,8 +13,10 @@ function About() {
         <Container>
           <div>
             <Row>
-              <Col md={12} >
-                <h1 id="about" className="col-sm-12"><span>About us</span></h1>
+              <Col md={12}>
+                <h1 id="about" className="col-sm-12">
+                  <span>About us</span>
+                </h1>
               </Col>
             </Row>
           </div>
@@ -23,64 +24,77 @@ function About() {
           <br />
           <div className="col-sm-12">
             <Row>
-              <Col md={6}  >
+              <Col md={6}>
                 <img src={about_1} alt="about_1" id="about_1" />
               </Col>
               <Col md={6} className="content">
-                <span>Blockenzyme, a dynamic tech outsourcing company founded by Swapnil Kole, is a visionary in the field of technology. We provide innovative technology solutions to businesses and organizations of all sizes, and are dedicated to sharing our expertise and knowledge through engaging and educational tech seminars.</span>
+                <span>
+                  Blockenzyme, a dynamic tech outsourcing company founded by
+                  Swapnil Kole, is a visionary in the field of technology. We
+                  provide innovative technology solutions to businesses and
+                  organizations of all sizes, and are dedicated to sharing our
+                  expertise and knowledge through engaging and educational tech
+                  seminars.
+                </span>
                 <br />
                 <br />
 
-                <span>At Blockenzyme, we believe that technology is a powerful tool that can revolutionize the way businesses operate. Our team is dedicated to delivering cutting-edge solutions that help our clients streamline their operations, increase efficiency, and stay ahead of the competition.</span>
+                <span>
+                  At Blockenzyme, we believe that technology is a powerful tool
+                  that can revolutionize the way businesses operate. Our team is
+                  dedicated to delivering cutting-edge solutions that help our
+                  clients streamline their operations, increase efficiency, and
+                  stay ahead of the competition.
+                </span>
               </Col>
             </Row>
-
             <br />
             <br />
-
             <Row id="reverse">
-
               <Col className="content col-md-6" id="content2">
-                Founded in 2023 , our team of experts has a wealth of experience in a wide range of technologies, including blockchain development, software engineering, and digital asset management. Our mission is to help businesses stay ahead of the curve in an ever-changing technological landscape.
+                Founded in 2023 , our team of experts has a wealth of experience
+                in a wide range of technologies, including blockchain
+                development, software engineering, and digital asset management.
+                Our mission is to help businesses stay ahead of the curve in an
+                ever-changing technological landscape.
               </Col>
               <Col className="col-md-6">
                 <img src={about_2} alt="about2" id="about_2" />
               </Col>
-
             </Row>
-
             <br />
-
             <Row>
               <Col md={6} sm={12}>
                 <img id="about_3" src={about_3} alt="about_3" />
-
-
               </Col>
               <Col md={6} sm={12} className="content">
-                <p>In addition to our tech outsourcing services, we also hold regular tech seminars led by industry experts, including our founder. These seminars provide a platform for attendees to learn about new technologies, network with other professionals, and stay up-to-date on the latest industry developments.
+                <p>
+                  In addition to our tech outsourcing services, we also hold
+                  regular tech seminars led by industry experts, including our
+                  founder. These seminars provide a platform for attendees to
+                  learn about new technologies, network with other
+                  professionals, and stay up-to-date on the latest industry
+                  developments.
                 </p>
-
                 <br />
-                <p>Join us on our mission to empower businesses and individuals with the knowledge and skills they need to succeed in a rapidly changing technological landscape. Contact us today to learn more about how we can help you stay ahead of the curve.</p>
-
+                <p>
+                  Join us on our mission to empower businesses and individuals
+                  with the knowledge and skills they need to succeed in a
+                  rapidly changing technological landscape. Contact us today to
+                  learn more about how we can help you stay ahead of the curve.
+                </p>
               </Col>
             </Row>
             <br />
-
           </div>
         </Container>
       </div>
-
       <br />
       <br />
       <div className="foot">
-
         <Footer />
-
       </div>
     </React.Fragment>
-
   );
 }
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,8 +2,5 @@ import Home from "./Home/Home";
 import Contact from "./ContactUs/Contact";
 import About from "./AboutUs/About";
 import Error from "./Error";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./AboutUs/About.styles.css";
-
 
 export {Error,Home,Contact,About};
